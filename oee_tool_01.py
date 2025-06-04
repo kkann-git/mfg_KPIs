9ea4b89031008fe47670f49b45b7a4a4715e6b7c
kkann-git <kevinintentionalmarketing+gh@gmail.com>
2025-06-04 17:18:03 -0400

    New files

--- oee_tool_01.py
+++ oee_tool_01.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 
 # Set page title and layout
 st.set_page_config(page_title="OEE Calculator", layout="centered")
